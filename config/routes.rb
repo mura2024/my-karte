f923be8c9401914964088cc5a54279c93ec995d9
mura2024 <ibuki.1377iv@gmail.com>
2023-10-30 22:37:33 +0900

    トップページ準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'exams/index'
+  root to: "exams#index"
 end
