67d12d6c4a0c36a8baecdbb492f714b59661fe21
mura2024 <ibuki.1377iv@gmail.com>
2023-11-13 22:14:11 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'exams/index'
+  # get 'exams/index'
   root to: "exams#index"
-  resources :users
+  resources :users, only: [:edit, :update]
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
