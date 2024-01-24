3d26997a74ac86d27ecd345e046d143b292d5b75
mura2024 <ibuki.1377iv@gmail.com>
2024-01-24 22:29:51 +0900

    examモデルのテスト

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,11 +3,11 @@ class Exam < ApplicationRecord
   belongs_to :medical
   has_one_attached :image
 
-  with_options presence: true, unless: :was_attached? do
-    validates :title
-    validates :hospital
-    validates :exam_date
-    validates :comment
+  with_options if: :was_attached? do
+    validates :title, presence: true
+    validates :hospital, presence: true
+    validates :exam_date, presence: true
+    validates :comment, presence: true
   end
 
   def was_attached?
